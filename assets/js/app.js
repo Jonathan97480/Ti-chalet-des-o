fab01023965f3831709959b3727a14fd62a60244
jonathan gauvin <dadou97480@hotmail.frgit>
2022-01-30 10:49:06 +0400

    fix header mobile and termined style menu mobile

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+const humberger=document.getElementById("humber");
+const menuMobile=document.getElementById('mobile-menu')
+
+humberger.addEventListener('click', () => {
+
+    menuMobile.className==="menu-item"? menuMobile.className="menu-item menu-active":menuMobile.className="menu-item"
+
+})
+
